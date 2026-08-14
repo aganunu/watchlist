b1e12e880cec231cc499d3065332290221969bcd
aganunu <gmtr200222@gmail.com>
2026-08-14 21:50:23 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,43 @@
-const CACHE='watchlist-v5-2';
-const CORE=['./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{
- const req=e.request;if(req.method!=='GET')return; const url=new URL(req.url);
- if(url.origin===location.origin && (req.mode==='navigate'||url.pathname.endsWith('/index.html')||url.pathname.endsWith('/'))){
-  e.respondWith(fetch(new Request(req,{cache:'no-store'})).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return res}).catch(()=>caches.match('./index.html')));return;
- }
- e.respondWith(caches.match(req).then(r=>r||fetch(req).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy));return res}).catch(()=>caches.match('./index.html'))));
+const CACHE='watchlist-v5-5';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
+
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  const req=event.request;
+  if(req.mode==='navigate'){
+    event.respondWith(
+      fetch(req,{cache:'no-store'})
+        .then(resp=>{
+          const copy=resp.clone();
+          caches.open(CACHE).then(c=>c.put('./index.html',copy));
+          return resp;
+        })
+        .catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+  event.respondWith(
+    fetch(req,{cache:'no-store'})
+      .then(resp=>{
+        if(resp.ok && new URL(req.url).origin===self.location.origin){
+          const copy=resp.clone();
+          caches.open(CACHE).then(c=>c.put(req,copy));
+        }
+        return resp;
+      })
+      .catch(()=>caches.match(req))
+  );
 });
